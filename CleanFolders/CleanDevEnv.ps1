413fd7a44ca791179d032365665b6ffcbad727d6
Joakim Brännström <tok_jocke@hotmail.com>
2014-04-09 22:55:18 +0200

    ...

--- CleanFolders/CleanDevEnv.ps1
+++ CleanFolders/CleanDevEnv.ps1
@@ -3,7 +3,7 @@ $RunSimulation = $true
 $Verbose = $true
 $FileTypesToRemove = @('*.dll', '*.pdb', '*.xml')
 $FolderNamesToInclude = @('bin', 'obj')
-$FolderNamesToIgnore = @('packages', '_tools$', '_build')
+$FolderNamesToIgnore = @('packages', '_tools$', '_build', 'MsBuild.Tasks')
 #$BasePath = (Get-Location -PSProvider FileSystem).ProviderPath
 #$BasePath = "D:\"
 
